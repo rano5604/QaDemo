241f609417bfb70dcd49701e03fe64c6366fce4b
rano5604 <rano5604@gmail.com>
2019-12-19 13:51:57 +0600

    master added 2nd commit

--- QaDemo/src/TestClassMaster.java
+++ QaDemo/src/TestClassMaster.java
@@ -3,6 +3,7 @@ public class TestClassMaster {
 	public static void main (String arg []){
 		System.out.println("Master Branch added this");
 		System.out.println("Demo1 branch added this");
+		System.out.println("master branch added again");
 	}
 
 }
